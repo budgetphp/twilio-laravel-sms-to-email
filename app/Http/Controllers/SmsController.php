e20a35bedbf86decad0fb59d119582815a976d6f
Jason Phillips <jason@budgetphp.com>
2022-09-03 21:48:14 -0400

    Update SmsController.php

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -4,7 +4,7 @@
  *
  * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
- * @author jason@budgetphp.com
+ * @author Jason Philips
  * @version 1.0
  * @since 09-31-2022
  * @category twilio
